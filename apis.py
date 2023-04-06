eed200d6659512b46f27663e6da5db75d3320c7f
Aditya0996 <aditya.somani30@gmail.com>
2023-04-06 03:09:13 -0400

    TicTacToe Api request handling

--- apis.py
+++ apis.py
@@ -2,6 +2,7 @@ import http.client
 import json
 import constants
 
+
 def getBoardStrings(gameId):
     board = []
     conn = http.client.HTTPSConnection("www.notexponential.com")
@@ -87,3 +88,68 @@ def getMoves(gameId, count):
     res = conn.getresponse()
     data = res.read()
     return json.loads(data.decode("utf-8"))["moves"][0]["moveId"]
+
+
+def createGame(team2, size=3, target=3):
+    conn = http.client.HTTPSConnection("www.notexponential.com")
+    dataList = []
+    boundary = 'wL36Yn8afVp8Ag7AmP8qZ0SA4n1v9T'
+    dataList.append('--' + boundary)
+    dataList.append('Content-Disposition: form-data; name=type;')
+
+    dataList.append('Content-Type: {}'.format('multipart/form-data'))
+    dataList.append('')
+
+    dataList.append("game")
+    dataList.append('--' + boundary)
+    dataList.append('Content-Disposition: form-data; name=teamId1;')
+
+    dataList.append('Content-Type: {}'.format('multipart/form-data'))
+    dataList.append('')
+
+    dataList.append(constants.teamId)
+    dataList.append('--' + boundary)
+    dataList.append('Content-Disposition: form-data; name=teamId2;')
+
+    dataList.append('Content-Type: {}'.format('multipart/form-data'))
+    dataList.append('')
+
+    dataList.append(str(team2))
+    dataList.append('--' + boundary)
+    dataList.append('Content-Disposition: form-data; name=gameType;')
+
+    dataList.append('Content-Type: {}'.format('multipart/form-data'))
+    dataList.append('')
+
+    dataList.append("TTT")
+    dataList.append('--' + boundary)
+    dataList.append('Content-Disposition: form-data; name=boardSize;')
+
+    dataList.append('Content-Type: {}'.format('multipart/form-data'))
+    dataList.append('')
+
+    dataList.append(str(size))
+    dataList.append('--' + boundary)
+    dataList.append('Content-Disposition: form-data; name=target;')
+
+    dataList.append('Content-Type: {}'.format('multipart/form-data'))
+    dataList.append('')
+
+    dataList.append(str(target))
+    dataList.append('--' + boundary + '--')
+    dataList.append('')
+    body = '\r\n'.join(dataList)
+    payload = body
+    headers = {
+        'x-api-key': constants.xapikey,
+        'userId': constants.userId,
+        'Content-type': 'multipart/form-data; boundary={}'.format(boundary)
+    }
+    conn.request("POST", "/aip2pgaming/api/index.php", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+
+    # print(data.decode("utf-8"))
+
+    ret = json.loads(data.decode("utf-8"))['gameId']
+    return ret
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
 
-from apis import getBoardStrings, makeMove, getMoves
+from apis import getBoardStrings, makeMove, getMoves, createGame
 from randomMove import random
 
 
@@ -13,13 +13,19 @@ def game(gameId):
 
 def main():
     flag = True
-    gameId = "3765"
+    team2id = 1362
+    gameId = 0
+    if gameId == 0:
+        gameId = createGame(team2id,6,4)
+        print(gameId)
     while flag:
         moveMade = game(gameId)
         if moveMade["code"] == "FAIL":
             if "Game is no longer open" in moveMade["message"]:
                 print("Game Over")
                 flag = False
+            else:
+                time.sleep(2)
         else:
             moveId = moveMade["moveId"]
             while moveId == getMoves(gameId, 1):
